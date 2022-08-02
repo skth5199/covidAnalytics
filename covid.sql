9a5e1046b292db9e09dc07010fe7ed7e4197bb29
Srikanth <srikanthtk99@gmail.com>
2022-08-02 19:57:24 +0100

    First commit

--- covid.sql
+++ covid.sql
@@ -50,3 +50,26 @@ from PortfolioProjects..covidDeaths
 where location like '%%' and continent is not null
 group by location
 ) as k;
+
+
+-- Covid Vaccinations Analysis
+-- Using a CTE
+
+With cumNewVac (continent, location, date, population, newvac, cumvac)
+as (
+select d.continent, d.location, d.date, d.population, v.new_vaccinations, SUM(CONVERT(int, v.new_vaccinations)) OVER (Partition by d.location order by d.location,d.date) as cummulativeVax
+from PortfolioProjects..covidDeaths d join PortfolioProjects..covidVax v
+	on d.location = v.location and d.date = v.date
+where d.continent is not null
+-- order by 2,3
+)
+select *,(cumvac/population)*100  from cumNewVac
+
+
+-- Creating a view for tableau for the above query
+create view cumVaxView as
+select d.continent, d.location, d.date, d.population, v.new_vaccinations, SUM(CONVERT(int, v.new_vaccinations)) OVER (Partition by d.location order by d.location,d.date) as cummulativeVax
+from PortfolioProjects..covidDeaths d join PortfolioProjects..covidVax v
+	on d.location = v.location and d.date = v.date
+where d.continent is not null
+--order by 2,3
